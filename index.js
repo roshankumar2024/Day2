694d4698c9a03c0070b48977af575cc112b437ff
Roshan Kumar <roshan.kumar@tudip.com>
2024-01-23 14:34:52 +0530

    commit code

---
+++ index.js
@@ -0,0 +1,24 @@
+// Array 
+
+let arr = [1,3,5,'Mango','Idli','Dosa',866,true]
+console.log(arr)
+
+let fruits = new Array('a','B','C','D')
+console.log(fruits)
+
+// Array methods 
+// pop method removes the last element from the array
+ let poppedelment = arr.pop
+ console.log(poppedelment)
+
+ // Push method
+ // Push method add an element to the end of an array
+
+ arr.push('jack')
+
+ var num = 67
+ var num3 = String(num)
+ console.log(num3)
+
+ var num2 = num3 + 5
+ console.log(num2)
